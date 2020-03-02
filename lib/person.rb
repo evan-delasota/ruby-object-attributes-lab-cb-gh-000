3c69324cff7cc887dac986d9359e5b25c4588b80
Evan Delasota <evan.delasota@gmail.com>
2020-03-02 02:39:30 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,17 +1,17 @@
 class Person
   def name=(new_name)
     @name = new_name
-  end 
+  end
 
   def name
-    @name 
-  end 
+    @name
+  end
 
   def job=(new_job)
     @job = new_job
-  end 
+  end
 
   def job
     @job
-  end 
+  end
 end 
